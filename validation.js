b7ba35706ea337943c8d19a3642a715312240a13
Abdumalik Karimov <39477628+Uzbekistanes@users.noreply.github.com>
2020-02-27 12:41:41 +0800

    uploaded

---
+++ validation.js
@@ -0,0 +1,31 @@
+// validation script here
+let reg = /[a-z]/i;
+
+// let regex = RegExp("[a-z]", "i");
+
+
+
+const inputs = document.querySelectorAll('input');
+
+const patterns = {
+	telephone:/^\d{11}$/,
+	username:/^[a-z\d]{5,12}$/,
+	password:/^[\w@-]{8,20}$/,
+	slug:/^[a-z\d-]{8,20}$/,
+	email:/^([a-z\d\.-]+)@([a-z]{2,8})(\.[a-z]{2,8})?$/
+};
+
+// validation function
+function validate(field, regex) {
+	if(regex.test(field.value)){
+		field.className = 'valid';
+	} else {
+		field.className = 'invalid';
+	}
+}
+
+inputs.forEach((input) => {
+	input.addEventListener('keyup', (e) => {
+		validate(e.target, patterns[e.target.attributes.name.value])
+	});
+});
